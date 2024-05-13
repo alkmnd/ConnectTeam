2783198e521c2d6078a195b2b6f2db3876752519
alkmnd <merch47@bwg-io.site>
2024-05-13 17:26:38 +0300

    Fix bags

--- pkg/repository/models/notification.go
+++ pkg/repository/models/notification.go
@@ -9,7 +9,7 @@ type Notification struct {
 	Type    string    `json:"type" redis:"type"`
 	Payload string    `json:"payload" redis:"payload"`
 	Date    time.Time `json:"date" redis:"date"`
-	IsRead  bool      `json:"is_read"`
+	IsRead  bool      `json:"is_read" redis:"is_read"`
 }
 
 func (n Notification) MarshalBinary() ([]byte, error) {
