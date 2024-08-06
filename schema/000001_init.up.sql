401a86e0ba77e503bf85873d7f8bc6ce960f64b3
alkmnd <merch47@bwg-io.site>
2024-08-06 20:49:40 +0300

    Change configs

--- schema/000001_init.up.sql
+++ schema/000001_init.up.sql
@@ -90,7 +90,7 @@ CREATE TABLE results
     id serial PRIMARY KEY,
     user_id uuid REFERENCES users (id) ON DELETE CASCADE,
     game_id uuid REFERENCES games (id) ON DELETE CASCADE,
-    name varchar(256)
+    name varchar(256),
     value int
 );
 
